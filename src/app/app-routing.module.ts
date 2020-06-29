bb07e8cc2470bfd5b79a9367999be1168324e777
Sasuke <c8763sasuke@gmail.com>
2020-06-29 17:03:27 +0800

    connect backend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
+import { ProductsComponent } from './products/products.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,21 @@
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  products$: Observable<Product[]>;
+
+  constructor(
+    private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.products$ = this.productService.getProducts();
+  }
+}
