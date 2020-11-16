df02625a1cf3e6349410e4d524de42f8729dd2fe
Hirito <c8763sasuke@gmail.com>
2020-11-16 16:39:54 +0800

    update header
    add signin button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,35 @@
 import { Observable } from 'rxjs';
 import { AccountService } from './account/account.service';
 import { Account } from './account/model/account.vm';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'shop';
+export class AppComponent implements OnInit {
+  title = 'GALA GALA';
 
   account$: Observable<Account>;
 
-  constructor(private accountService: AccountService) {
+  constructor(
+    private accountService: AccountService,
+    private router: Router) {
+
+  }
+
+  ngOnInit(): void {
     this.account$ = this.accountService.getAccount();
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigate(['/account']);
+  }
+
+  signin() {
+    this.router.navigate(['/account']);
   }
 }
