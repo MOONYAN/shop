76de2226391d6a6dee546fac3c99ed4ed086c176
Hirito <c8763sasuke@gmail.com>
2020-11-17 13:26:23 +0800

    implement sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
